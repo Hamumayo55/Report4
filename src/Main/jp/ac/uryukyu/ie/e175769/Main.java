c8a42e1888556f1160dd5867b52630c7a307dd4a
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2017-11-19 16:07:33 +0900

    step2: 例外処理を記述せよ

--- src/Main/jp/ac/uryukyu/ie/e175769/Main.java
+++ src/Main/jp/ac/uryukyu/ie/e175769/Main.java
@@ -3,8 +3,9 @@ package jp.ac.uryukyu.ie.e175769;
 public class Main {
     public static void main(String[] args){
         try {
-            String str = null;
-            System.out.println(str.length());
+            String str = "3．14";
+            //System.out.println(str.length());
+            double value = Double.parseDouble(str);
         }
         catch(NullPointerException e){
             System.out.println(e.toString()+"が発生");
